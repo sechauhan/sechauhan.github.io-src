0f2c3b5a17afb35223bfe352e7d45727905d5a3e
se.chauhan <seema.chauhan@geminisolutions.in>
2018-07-25 16:48:16 +0530

    site url

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://sechauhan.github.io'
+SITEURL = 'https://sechauhan.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
